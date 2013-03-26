1399c8047855aa0b94b9400dcf9719b6eaf5b4c4
William O'Hanley <willy.ohanley@gmail.com>
2013-03-25 22:15:20 -0300

    Initial commit

---
+++ pic_decode.py
@@ -0,0 +1,34 @@
+'''
+Created on Sep 12, 2011
+
+@author: Willy
+'''
+
+import png
+import sys
+
+filename = sys.argv[1]
+
+# Get the image data
+if filename[-3:].lower() == "png":
+    picture = png.Reader(filename)
+    
+imageData = picture.read_flat()
+pixels = imageData[2]
+
+message = ""
+currentChar = ord(' ')
+charCount = 0
+
+while currentChar != ord('\0') and charCount < 50:
+    binString = ""
+    for i in xrange(0, 8):
+        if pixels[charCount * 8 + i] % 2 == 0:
+            binString += '0'
+        else:
+            binString += '1'
+    currentChar = int(binString, 2)
+    message += chr(currentChar)
+    charCount += 1
+    
+print message
---
+++ pic_encode.py
@@ -0,0 +1,40 @@
+'''
+Created on Sep 12, 2011
+
+@author: Willy
+'''
+
+import png
+import sys
+
+message = raw_input("Enter your message:\n") + '\0'
+filename = sys.argv[1]
+outFilename = sys.argv[2]
+
+# Get the image data
+if filename[-3:].lower() == "png":
+    picture = png.Reader(filename)
+    
+imageData = picture.read_flat()
+pixels = imageData[2]
+
+# Convert the message to a sequence of ordinal values
+chars = []
+for ch in message:
+    chars.append(ord(ch));
+
+charCount = 0
+for ordinal in chars:
+    pixelPosition = charCount * 8
+    for i in xrange(7, -1, -1):
+        if ordinal % 2 == 0:
+            pixels[pixelPosition + i] &= 0B11111110
+        else:
+            pixels[pixelPosition + i] |= 1
+        ordinal = ordinal // 2
+    charCount += 1
+    
+outFile = open(outFilename, 'wb')
+writer = png.Writer(514, 291)
+writer.write_array(outFile, pixels)
+outFile.close()
